a64b2bd24094524b58c00469fc7c6f442f255d4f
Ruizaius <67200522+Ruizaius@users.noreply.github.com>
2022-04-23 16:09:53 +0100

    added UNI project

--- firstapp/lib/Data/Cadeiras.dart
+++
@@ -1,8 +0,0 @@
-/* Esta função deve conseguir encontrar todas as cadeiras
-associadas a um aluno no semestre presente*/
-
-//PlaceHolder
-
-List<String> getCadeiras() {
-  return <String>['ES', 'DA', 'LTW', 'LC'];
-}
